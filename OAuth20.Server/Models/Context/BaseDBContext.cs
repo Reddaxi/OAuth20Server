1792936acda84b6d6844e3ed8179afde9d4c45f2
Mathias Blaaberg <blaaber@live.dk>
2023-04-04 07:45:09 +0200

    Copied dbcontext config from WoWMotivator

--- OAuth20.Server/Models/Context/BaseDBContext.cs
+++ OAuth20.Server/Models/Context/BaseDBContext.cs
@@ -9,15 +9,31 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using OAuth20.Server.Models.Entities;
+using System;
 
 namespace OAuth20.Server.Models.Context
 {
     public class BaseDBContext : IdentityDbContext<AppUser>
     {
+        public string DbPath { get; set; }
         public BaseDBContext(DbContextOptions<BaseDBContext> options) : base(options)
         {
-
+            setDbPath();
         }
+
+        private void setDbPath()
+        {
+            string path = "";
+            var folder = Environment.SpecialFolder.CommonApplicationData;
+            path = Environment.GetFolderPath(folder);
+            DbPath = System.IO.Path.Join(path, "OAuth2Server_DB/OAuth2Server.db");
+        }
+
+        // The following configures EF to create a Sqlite database file in the
+        // special "local" folder for your platform.
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite($"Data Source={DbPath}");
+
         public DbSet<OAuthApplicationEntity> OAuthApplications { get; set; }
         public DbSet<OAuthTokenEntity> OAuthTokens { get; set; }
     }
